afd8d73dc42f4c88ed5bdc9770d8337d3ddc8be0
tamsamani <tamsamani@users.noreply.github.com>
2021-07-29 17:34:50 +0000

    Build Dist Files

---
+++ dist/core/450.js
@@ -0,0 +1,19 @@
+"use strict";
+(self["webpackChunkTF"] = self["webpackChunkTF"] || []).push([[450],{
+
+/***/ 450:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ install)
+/* harmony export */ });
+async function install(settings) {
+  // log install sucess
+  console.log("install success");
+}
+
+
+/***/ })
+
+}]);
