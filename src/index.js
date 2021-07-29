18eb29510ce75f01a282bc63c5ce173072a307a9
Mohamed Tamsamani <mohamed@tousfacteurs.com>
2021-07-29 09:39:28 +0100

    Look for node traspile

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("ACCESSED");
+const package = require("./package.json");
+
+console.log("Tousfactor: ", package.name, "v" + package.version);
+// check if code run on browsers
+if (typeof window !== "undefined") {
+} else {
+  // warn the current system is not supported
+  console.log("The current system is not supported.");
+}
